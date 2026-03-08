f58c8814132fde10b555845f19e369567a1d302e
Varasiri Limprasert <bearishstaff@gmail.com>
2026-03-08 11:59:06 +0700

    init project and add health check

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sarinthip-backend
+go mod init sarinthip-backend
+go get github.com/gin-gonic/gin gorm.io/gorm gorm.io/driver/postgres github.com/google/uuid
---
+++ cmd/api/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 1. Init Database
+	// database.Connect()
+
+	// 2. Init Gin Router
+	r := gin.Default()
+
+	// 3. Setup CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://https://sarinthip-web.vercel.app"}, // web
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
+	// 4. Routes
+	api := r.Group("/api/v1")
+	{
+		api.GET("/health", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"status": "ok",
+			})
+		})
+		// Branch Management
+		// api.GET("/branches", handlers.GetBranches)
+
+		// Expense & Bill Management
+		// api.POST("/parse", handlers.ParseRawText)    // Returns JSON for preview
+		// api.POST("/bills", handlers.CreateBillGroup) // Saves Bill + many Expenses
+
+		// Income Management
+		// api.POST("/income", handlers.CreateIncome) // Saves Income record
+	}
+
+	r.Run(":8080") // Railway/Render will detect this port
+}
